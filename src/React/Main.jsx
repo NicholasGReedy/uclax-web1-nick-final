1c12371f5a5d0bf894fe1e830ba7cbd57d6f3e64
Nick Reedy <nicholas.g.reedy@gmail.com>
2021-01-06 19:49:07 -0800

    My first commit

---
+++ src/React/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/* Components ---------------------------*/
+import Test from './Test.jsx';
+
+const Main = () => {
+
+    return (
+        <div className='Main'>
+            <h1>Nick's Final </h1>
+        </div>
+    );
+}
+
+export default Main;
